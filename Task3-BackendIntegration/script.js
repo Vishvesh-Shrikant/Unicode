1e1a864cc09403eeb4c4a7982dc4906afdbf6974
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-07-26 09:56:58 +0530

    added functionality to the UI and integrated with backend with changes

--- Task3-BackendIntegration/script.js
+++ Task3-BackendIntegration/script.js
@@ -1,11 +1,68 @@
 const API='https://se-tasks.vercel.app/events'
 
 const container=document.querySelector(".card-container")
+const singleEvent=document.querySelector(".single-event")
+const singleEventContainer=document.querySelector(".details-container")
 //Fetching data from the API 
 const getData=async ()=>{
     let response= await fetch(API)
     let data=await response.json()
     printData(data)
+    
+}
+
+const selectedData=(data, id, textColour)=>
+{
+    console.log("Hello World")
+    for(let i=0; i<data.length;i++)
+    {
+        if(data[i]._id==id)
+        {
+            singleEvent.style.display="block"
+            singleEventContainer.style.color=textColour
+            const eventName=data[i].name
+            const eventDate=data[i].date.slice(0,10)
+            const eventTime=data[i].time
+            const eventLocation=data[i].venue
+            const eventDescription=data[i].description
+            const eventPrice=data[i].ticketPrice
+            const eventTickets=data[i].availableTickets
+
+           singleEventContainer.innerHTML=
+            `<p class="singleEvent-name">${eventName}</p>
+              <p class="event desc">${eventDescription}</p>
+                <div class="singleEvent">
+                     <i class="fa-regular fa-calendar-days"></i>
+                    <p class="data">${eventDate}</p>
+                </div>
+                <div class="singleEvent">
+                    <i class="fa-regular fa-clock"></i>
+                     <p class="data">${eventTime}</p>
+                </div>
+                <div class="singleEvent">
+                    <i class="fa-solid fa-map-pin"></i>
+                    <p class="data">${eventLocation}</p>
+                </div>
+                <div class="singleEvent ">
+                    <i class="fa-solid fa-money-bill"></i>
+                    <p class="data">$${eventPrice}</p>
+                </div>
+                <div class="singleEvent ">
+                    <i class="fa-solid fa-ticket"></i>
+                    <p class="data">${eventTickets} Available</p>
+                </div>
+                <div class="close">
+                    <i class="fa-solid fa-xmark"></i>
+                 </div>`
+            
+            const close=document.querySelector(".close")
+            close.addEventListener("click",()=>{
+                singleEvent.style.display="none"
+            })
+        }
+        else
+        continue
+    }  
 }
 
 //Displaying Name, time , date and location 
@@ -16,9 +73,6 @@ const printData=(data)=>{
         const eventDate=data[i].date.slice(0,10)
         const eventTime=data[i].time
         const eventLocation=data[i].venue
-        const eventDescription=data[i].description
-        const eventPrice=data[i].price
-        const eventTickets=data[i].availableTickets
 
         if(i%3==0)
         {
@@ -26,7 +80,6 @@ const printData=(data)=>{
             card.classList.add("card")
             card.innerHTML=
               `<p class="event-name">${eventName}</p>
-              <p class="event desc">${eventDescription}</p>
                 <div class="event">
                      <i class="fa-regular fa-calendar-days"></i>
                     <p class="data">${eventDate}</p>
@@ -39,37 +92,11 @@ const printData=(data)=>{
                     <i class="fa-solid fa-map-pin"></i>
                     <p class="data">${eventLocation}</p>
                 </div>
-                <div class="event money">
-                    <i class="fa-solid fa-money-bill"></i>
-                    <p class="data">$${eventPrice}</p>
-                </div>
-                <div class="event tickets">
-                    <i class="fa-solid fa-ticket"></i>
-                    <p class="data">${eventTickets} Available</p>
-                </div>`
+                `
             container.appendChild(card)
-            const desc=card.querySelector(".desc")
-            const money=card.querySelector(".money")
-            const tickets=card.querySelector(".tickets")
-
-            desc.style.display ="none"
-            money.style.display="none"
-            tickets.style.display="none"
-
             card.addEventListener("click",()=>{
-                card.classList.toggle("clicked")
-                if(card.classList.contains("clicked")) {
-                    desc.style.display="flex" 
-                    money.style.display="flex"
-                    tickets.style.display="flex"
-                    card.style.height="100%"
-                }
-                else
-                {
-                    desc.style.display ="none"
-                    money.style.display="none"
-                    tickets.style.display="none"
-                }
+                window.scrollTo(0,0)
+                selectedData(data, data[i]._id,"#08A4BD")
             })
         }
         if(i%3==1)
@@ -78,7 +105,6 @@ const printData=(data)=>{
             card.classList.add("card2")
             card.innerHTML=
               `<p class="event-name">${eventName}</p>
-              <p class="event desc">${eventDescription}</p>
                 <div class="event">
                      <i class="fa-regular fa-calendar-days"></i>
                     <p class="data">${eventDate}</p>
@@ -90,38 +116,12 @@ const printData=(data)=>{
                 <div class="event">
                     <i class="fa-solid fa-map-pin"></i>
                     <p class="data">${eventLocation}</p>
-                </div>
-                <div class="event money">
-                    <i class="fa-solid fa-money-bill"></i>
-                    <p class="data">$${eventPrice}</p>
-                </div>
-                <div class="event tickets">
-                    <i class="fa-solid fa-ticket"></i>
-                    <p class="data">${eventTickets} Available</p>
                 </div>`
             container.appendChild(card)
-            const desc=card.querySelector(".desc")
-            const money=card.querySelector(".money")
-            const tickets=card.querySelector(".tickets")
-
-            desc.style.display ="none"
-            money.style.display="none"
-            tickets.style.display="none"
-
             card.addEventListener("click",()=>{
-                card.classList.toggle("clicked")
-                if(card.classList.contains("clicked")) {
-                    desc.style.display="flex" 
-                    money.style.display="flex"
-                    tickets.style.display="flex"
-                    card.style.height="100%"
-                }
-                else
-                {
-                    desc.style.display ="none"
-                    money.style.display="none"
-                    tickets.style.display="none"
-                }
+                window.scrollTo(0,0)
+                selectedData(data, data[i]._id,"#D1E3DD")
+
             })
         }
         if(i%3==2)
@@ -130,7 +130,6 @@ const printData=(data)=>{
             card.classList.add("card3")
             card.innerHTML=
               `<p class="event-name">${eventName}</p>
-              <p class="event desc">${eventDescription}</p>
                 <div class="event">
                      <i class="fa-regular fa-calendar-days"></i>
                     <p class="data">${eventDate}</p>
@@ -142,38 +141,11 @@ const printData=(data)=>{
                 <div class="event">
                     <i class="fa-solid fa-map-pin"></i>
                     <p class="data">${eventLocation}</p>
-                </div>
-                <div class="event money">
-                    <i class="fa-solid fa-money-bill"></i>
-                    <p class="data">$${eventPrice}</p>
-                </div>
-                <div class="event tickets">
-                    <i class="fa-solid fa-ticket"></i>
-                    <p class="data">${eventTickets} Available</p>
                 </div>`
             container.appendChild(card)
-            const desc=card.querySelector(".desc")
-            const money=card.querySelector(".money")
-            const tickets=card.querySelector(".tickets")
-
-            desc.style.display ="none"
-            money.style.display="none"
-            tickets.style.display="none"
-
             card.addEventListener("click",()=>{
-                card.classList.toggle("clicked") 
-                if(card.classList.contains("clicked")) {
-                    desc.style.display="flex" 
-                    money.style.display="flex"
-                    tickets.style.display="flex"
-                    card.style.height="100%"
-                }
-                else
-                {
-                    desc.style.display ="none"
-                    money.style.display="none"
-                    tickets.style.display="none"
-                }
+                window.scrollTo(0,0)
+                selectedData(data, data[i]._id,"#FAA381")
             })
         }
     }
