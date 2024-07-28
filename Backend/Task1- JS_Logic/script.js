e06cbda3a148537cba0eacac36f142e2ee7d5cbf
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-07-28 18:28:19 +0530

    updating to a refined logic

--- Backend/Task1- JS_Logic/script.js
+++ Backend/Task1- JS_Logic/script.js
@@ -3,33 +3,30 @@ console.log("Iput string: "+str)
 str=str.toLowerCase()
 str=str.replace(" ","")
 
-let freq=""
+let freq1=""
 let freq2=""
-let str_arr=[]
-//gets aray of unique characters
+let count=0 
+//count stores value of number of unique characters
+const char_arr={}
+//creating objects of unique characters
 for(let ch of str)
 {
-    if(ch==' ')
-        continue
-    else if(!str_arr.includes(ch))
-        str_arr.push(ch)
-}
-
-//checks for frequency
-for(let i of str_arr)
-{
-    let count=0
-    for(let ch of str)
-    {   
-        if(ch==i)
-            count++
+    //if charcter exists in object , increment value by 1 
+    if(char_arr[ch])
+        char_arr[ch]+=1
+    //else add character to the object with initial value, and incremenet count by 1
+    else{
+        count++;
+        char_arr[ch]=1
     }
-    freq+= count+" "
-    freq2+=i+":"+count+" "
 }
 
 //prints required results 
-console.log(str_arr.length)
-console.log(freq)
+console.log(count)
+for(let key in char_arr)
+{
+    freq1+=char_arr[key]+" "
+    freq2+=key+":"+char_arr[key]+" "
+}
+console.log(freq1)
 console.log(freq2)
-
